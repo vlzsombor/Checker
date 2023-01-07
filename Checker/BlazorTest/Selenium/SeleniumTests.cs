8a7343ccb45e55d0705c8cba0250c560b7b80fff
Zsombor Veres-Lakos <vl.zsombor@hotmail.com>
2023-01-07 11:43:42 +0100

    feat: selenium test poc

--- Checker/BlazorTest/Selenium/SeleniumTests.cs
+++ Checker/BlazorTest/Selenium/SeleniumTests.cs
@@ -1,35 +1,55 @@
 ﻿using System;
+using System.Linq;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using static System.Net.WebRequestMethods;
+using SeleniumExtras.WaitHelpers;
 
-namespace BlazorTest.Selenium
+namespace BlazorTest.Selenium;
+
+public class SeleniumTests : IDisposable
 {
-	public class SeleniumTests : IDisposable
-	{
-        private readonly IWebDriver driver;
+    private readonly IWebDriver driver;
 
-        public SeleniumTests()
-		{
-			driver = new Driver().GetDriver();
-            driver.Navigate().GoToUrl(new Uri("http://google.com"));
-        }
+    public SeleniumTests()
+    {
+        driver = new Driver().GetDriver();
+        driver.Navigate().GoToUrl(new Uri("http://localhost:49835/"));
 
 
-        [Fact]
-        public void Test1()
-        {
-            //driver.FindElement(By.CssSelector)
 
-            driver.FindElement(By.CssSelector("#L2AGLb > div")).Click();
-            driver.FindElement(By.CssSelector("body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.FPdoLc.lJ9FBc > center > input.RNmpXc")).Click();
+    }
+
+    [Fact]
+    public void Test1()
+    {
+        //driver.FindElement(By.CssSelector)
+        //driver.FindElement(By.CssSelector)
 
 
-            Assert.True(false);
-        }
-        
-        public void Dispose()
-        {
-            driver.Quit();
-        }
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(20));
+        var div = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("createGame")));
+
+        driver.FindElement(By.Id("createGame")).Click();
+
+        ((IJavaScriptExecutor)driver).ExecuteScript($"window.open('{driver.Url}');");
+        driver.SwitchTo().Window(driver.WindowHandles.Last());
+        var div2 = wait.Until(ExpectedConditions.ElementIsVisible(By.Id("joinTable")));
+
+        driver.FindElement(By.Id("joinTable")).Click();
+
+        driver.SwitchTo().Window(driver.WindowHandles.First());
+
+
+        driver.FindElement(By.Id("cellId5-0")).Click();
+
+
+        Assert.True(false);
+    }
+
+    public void Dispose()
+    {
+        driver.Quit();
     }
 }
 
